10f6893036b3b151bfdf34d52b274669022541d8
Man-from-earth25 <manfromearth25@gmail.com>
2022-06-26 01:41:52 +0600

    Hi fumaswap by Manfromearth25

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -54,7 +54,7 @@ function MyApp(props: AppProps) {
         />
        <title>Fumaswap</title>
        
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Man-from-earth25/fiverr-milkyswap-website/milkyswap_config_1o4.css"/>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Man-from-earth25/fiverr-milkyswap-website/milkyswap_config_1o5.css"/>
        
       </Head>
       <Providers store={store}>
